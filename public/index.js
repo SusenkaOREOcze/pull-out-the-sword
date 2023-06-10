11ea69380949b90f2227d2b75b6eab95cda161fa
SusenkaOREOcz <humorousgmsvidea@gmail.com>
2023-06-10 22:23:24 +0200

    updated textures, code, and gameplay

--- public/index.js
+++ public/index.js
@@ -1,33 +1,31 @@
 const sword = document.getElementById('sword');
 let countText = document.getElementById('count');
 
-countText.textContent = 1000000000000000;
-
 var isClicked = false;
 
 sword.onclick = moveSword;
 
 async function moveSword() {
-if (isClicked) {
-    return;
-} else {
-    isClicked = true;
-    sword.onclick = null;
+    if (isClicked) {
+        return;
+    } else {
+        isClicked = true;
+        sword.onclick = null;
 
-    sword.style.transform = 'rotate(181deg)';
+        sword.style.transform = 'rotate(1deg)';
 
-    await sleep(250);
-    sword.style.transform = 'rotate(179deg)';
+        await sleep(270);
+        sword.style.transform = 'rotate(-1deg)';
 
-    await sleep(250);
-    sword.style.transform = 'rotate(180deg)';
+        await sleep(270);
+        sword.style.transform = 'rotate(0deg)';
 
-    sendMsg(1);
+        sendMsg(1);
 
-    isClicked = false;
-    sword.onclick = moveSword;
-    return;
-}
+        isClicked = false;
+        sword.onclick = moveSword;
+        return;
+    }
 }
 
 function sleep(ms) {
--- server.js
+++ server.js
@@ -1,37 +1,36 @@
 const WebSocket = require("ws");
 const express = require("express");
-const app = express()
 const path = require("path");
+require('dotenv').config()
 
-var goal = 1000000000000000;
+// set variables for data
+var goal = Number(process.env.GOAL);
 
+// set up express server
+const app = express()
 app.use("/",express.static(path.resolve(__dirname, "./public")));
 
-const myServer = app.listen(3000);       // regular http server using node express which serves your webpage
+const server = app.listen(process.env.PORT, () => {
+    console.log("Server started on port " + process.env.ROUTE + ":" + process.env.PORT);
+});
 
+// set up websocket server
 const wsServer = new WebSocket.Server({
-    server: myServer                    // no need to create a new server
-})                                      // a websocket server
-
-wsServer.on("connection", function(ws) {    // what should a websocket do on connection
-    ws.send(goal);
-  
-  ws.on("message", function(msg) {
-        goal -= msg;
-      
-        wsServer.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {  
-                client.send(goal)
-              
-            }
-        })
-    });
+    server: server
 })
 
-// myServer.on('upgrade', async function upgrade(request, socket, head) {      //handling upgrade(http to websocekt) event
-    
-//     //emit connection when request accepted
-//     wsServer.handleUpgrade(request, socket, head, function done(ws) {
-//       wsServer.emit('connection', ws, request);
-//     });
-// });
+// handle websocket events
+wsServer.on("connection", function(ws) {
+    ws.send(goal);
+
+    ws.on("message", function(msg) {
+        goal -= msg;
+
+        // for each client connected to the server
+        wsServer.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {  
+                client.send(goal);
+            };
+        });
+    });
+});
